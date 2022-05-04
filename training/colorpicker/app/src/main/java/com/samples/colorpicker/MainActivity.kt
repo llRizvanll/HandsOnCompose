2acfafdf68a073ea7ab1d23a352ecc8de1543453
Rizvan <rizvan@Rizvans-MacBook-Pro-2.local>
2022-05-04 18:57:50 +0530

    color picker project added

---
+++ training/colorpicker/app/src/main/java/com/samples/colorpicker/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.samples.colorpicker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import com.samples.colorpicker.ui.theme.ColorPickerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ColorPickerTheme {
+                BoxWithConstraints(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Column (
+                        modifier = Modifier.width(min(400.dp,maxWidth)),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        val color = remember {
+                            mutableStateOf(Color.Magenta)
+                        }
+                        ColorPicker(color)
+                        Text(
+                            text = "#${color.value.toArgb().toUInt().toString(16)}",
+                            modifier = Modifier.fillMaxWidth().background(color.value),
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.h4.merge(
+                                TextStyle(
+                                    color = color.value.complementary()
+                                )
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ColorPickerTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun ColorPicker(color:MutableState<Color>){
+    val red = color.value.red
+    val green = color.value.green
+    val blue = color.value.blue
+
+    Column {
+        Slider(
+            value = red,
+            onValueChange = {color.value =  Color(it, green ,blue)})
+        Slider(
+            value = green,
+            onValueChange = {color.value =  Color(red,it ,blue)})
+        Slider(
+            value = blue,
+            onValueChange = {color.value =  Color(red, green ,it)})
+    }
+
+}
+
+fun Color.complementary() = Color (
+    red = 1F - red,
+    green = 1F - green,
+    blue = 1F - blue
+        )
